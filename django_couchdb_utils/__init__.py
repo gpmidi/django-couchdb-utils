162e6f15520dadd8569905ca5465ec5c3adda25a
Stefan Kögl <stefan@skoegl.net>
2013-12-30 11:54:33 +0100

    bump version to 0.5

--- django_couchdb_utils/__init__.py
+++ django_couchdb_utils/__init__.py
@@ -5,7 +5,7 @@
 the relational databases supported by Django's ORM. """
 
 __author__ = 'Stefan Kögl <stefan@skoegl.net>'
-__version__ = '0.4'
+__version__ = '0.5'
 __website__ = 'https://github.com/stefankoegl/django-couchdb-utils'
 __license__ = 'Modified BSD License'
 
